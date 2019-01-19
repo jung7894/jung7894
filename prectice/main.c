6e081cdb66b39215b05718b25f38931552ac74c1
송준섭 <songjunseob@songjunseob-ui-macbook-pro.local>
2019-01-19 16:42:11 +0900

    Initial Commit

---
+++ prectice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  prectice
+//
+//  Created by 송준섭 on 19/01/2019.
+//  Copyright © 2019 Song. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
